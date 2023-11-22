6701ff0281128dbae4c59fd9c71c4dbaf025923a
Gleidson Viana <gleidson.social@outlook.com>
2023-11-21 22:58:53 -0300

    first commit

---
+++ css/home.css
@@ -0,0 +1,90 @@
+/* Estilo para o cabeçalho */
+header {
+  background-color: #007bff;
+  color: white;
+  text-align: center;
+  padding: 20px;
+  font-family: "Roboto", sans-serif;
+}
+
+header h1 {
+  font-size: 32px;
+  font-family: "Roboto", sans-serif;
+}
+
+nav ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+nav li {
+  display: inline;
+  margin-right: 20px;
+}
+
+nav a {
+  text-decoration: none;
+  color: white;
+  font-weight: bold;
+}
+
+/* Estilo para todas as imagens */
+img {
+  width: 100px;
+}
+
+/* Estilo para o conteúdo principal */
+main {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+  font-family: "Roboto", sans-serif;
+}
+
+.destaque {
+  background-color: #f8f8f8f5;
+  padding: 20px;
+  border-radius: 5px;
+}
+
+.destaque h2 {
+  font-size: 24px;
+  color: #007bfff8;
+}
+
+.conteudo-destaque {
+  margin-top: 20px;
+  font-family: "Roboto", sans-serif;
+}
+
+.conteudo-destaque a {
+  display: block;
+  text-decoration: none;
+  border: 1px solid #ddddddf6;
+  border-radius: 5px;
+  padding: 10px;
+  text-align: center;
+  color: #333333e8;
+}
+
+.conteudo-destaque h3 {
+  font-size: 18px;
+  margin: 10px 0;
+}
+
+/* Estilo para o rodapé */
+footer {
+  background-color: #007bff;
+  color: white;
+  text-align: center;
+  padding: 10px;
+}
+
+/* Estilo para links */
+a {
+  color: #007bff;
+}
+
+a:hover {
+  text-decoration: underline;
+}
